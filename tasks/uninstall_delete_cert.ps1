44e99d568a97c2ac76f03093cabff2c20c1eb2b2
Benjamin Robertson <ben.robertson@puppet.com>
2021-08-03 13:45:23 +1000

    Updated to include windows custom cert

---
+++ tasks/uninstall_delete_cert.ps1
@@ -0,0 +1,3 @@
+Uninstall-Package -Force -Name 'Puppet Agent (64-bit)'
+rm -Recurse 'C:\Program Files\Puppet Labs'
+rm -Recurse C:\ProgramData\PuppetLabs\puppet
--- tasks/uninstall_delete_cert.sh
+++ tasks/uninstall_delete_cert.sh
@@ -24,11 +24,8 @@
 
 
 # set all my varibles
-$pe_server=$env:PT_new_pe_server
+pe_server=$PT_new_pe_server
 
-echo $pe_server > c:\varibles.txt
-
-Uninstall-Package -Force -Name 'Puppet Agent (64-bit)'
-rm -Recurse 'C:\Program Files\Puppet Labs'
-rm -Recurse C:\ProgramData\PuppetLabs\
-[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile("https://$pe_server:8140/packages/current/install.ps1", 'install.ps1'); .\install.ps1 -v
+yum -y erase puppet-agent
+rm -rf /etc/puppetlabs/
+rm -rf /opt/puppetlabs/puppet
